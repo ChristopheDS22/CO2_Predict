d07cb221fe26c0678868d30938f47e0b3e1ab568
camillemillon <113453113+camillemillon@users.noreply.github.com>
2022-10-03 17:17:32 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,22 +10,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-
+import plotly.graph_objects as go
 import streamlit as st
+
 
 # Titre du streamlit
 st.title('Projet CO2 Predict')
 
 # Sommaire du Sreamlit
 st.sidebar.title('Sommaire')
-pages = ['Introduction','Exploration des données', 'Analyse de données', 'Dataviz', 
+pages = ['Introduction','Exploration et analyse des données', 'Analyse de données - à supprimer', 'Dataviz - à supprimer', 
          'Modélisation : Classification multiple', 'Modélisation : Regréssion']
 
 page = st.sidebar.radio('Aller vers', pages)
 
 # Emissions de polluants, CO2 et caractéristiques des véhicules
 # commercialisés en France en 2013
-df_2013 = pd.read_csv('data_2013.csv',encoding='latin-1',sep=';')
+df_2013 = pd.read_csv('data_2013.csv' , sep = ';', encoding='unicode_escape')
 
 st.write('### Visualisation du dataset')
 st.dataframe(df_2013.head())
@@ -41,24 +42,13 @@ if page == pages[0]:
 if page == pages[1]:
     st.write('## Exploration des données')
     
-    st.markdown('La variable cible est CO2 (g/km)')
+    st.markdown('La variable cible est CO2 (g/km). Elle peut être expliquée en tant que variable continue ou discrète')
     #st.markdown('Il y a ', df_2013.duplicated().sum(), 'doublons dans le dataset') # On checke les doublons
-    
-    st.markdown('Les variables explicatives sont : LISTE VARIABLES QUALIT ET QUANTI à regrouper par "famille"')
     
     # Suppression des doublons
     df_2013 = df_2013.drop_duplicates()
     df_2013.duplicated().sum()
     
-    st.markdown('Traitement des valeurs manquantes')
-   
-    st.write('### Etat des lieux des données')
-    st.dataframe(df_2013.info())
-    st.markdown('Beaucoup de valeurs manquantes pour les variables HC (g/km) et HC+NOX (g/km)')
-    
-    
-    if st.checkbox('Afficher les valeurs manquantes'):
-        st.dataframe(df_2013.isna().sum())    
     
     # Remplacement des valeurs manquantes des 'EL' par 0:
     df_2013['CO2 (g/km)'] = df_2013['CO2 (g/km)'].fillna(0)
@@ -80,9 +70,7 @@ if page == pages[1]:
     # On vérifie le remplacement des valeurs manquantes des variables numériques
     df_2013.isna().sum()
     
-    st.write('#### Description statistique des données')
-    st.dataframe(df_2013.describe())
-    st.markdown('Les ordres de grandeurs sont très larges : il sera nécessaire de procéder à une standardisation des valeurs')
+
     
     # Harmonisation des nomenclatures de carburants
     df_2013['Carburant'] = df_2013['Carburant'].replace(to_replace = ['GN/ES', 'GP/ES'], value = ['ES/GN', 'ES/GP'])
@@ -94,12 +82,7 @@ if page == pages[1]:
     # Extraction des variables quantitatives
     df_quant = df_2013[['Puissance administrative', 'Puissance maximale (kW)', 'Consommation urbaine (l/100km)', 'Consommation extra-urbaine (l/100km)', 'Consommation mixte (l/100km)', 'CO2 (g/km)', 'CO type I (g/km)', 'Particules (g/km)', 'NOX (g/km)', 'masse vide euro min (kg)', 'masse vide euro max (kg)']]
 
-    
-    st.markdown('#### Corrélation des variables selon Pearson')
-    st.dataframe(df_quant.corr())
-    st.markdown("Il apparaît qu'il y a une forte corrélation entre les différents types de consommation et les émissions de CO2")
-    st.markdown("Il y a aussi une corrélation marquée entre les masses des véhicules (min et max) et les émissions de CO2")
-    st.markdown("La variable HC+NOX (g/km) n'a pas été retenue car elle a trop de valeurs manquantes")
+
     
     # Création d'une variable catégorielle 'Cat' selon la norme européenne de pollution des véhicules:
 
@@ -126,48 +109,97 @@ if page == pages[1]:
     # Création d'une colonne 'Cat' dans le DataFrame df_2013:
     df_2013['Cat'] = A 
 
-#------------------------------------------------------------------------
-#------------------------------------------------------------------------
+#------------------------------
+#partie streamlit des représentations
+#------------------------------
+
+    st.write('### Variable à expliquer')
+
+
+    fig=px.box(df_2013['CO2 (g/km)'],x='CO2 (g/km)',title='Variable CO2 continue',notched=True)
+    st.plotly_chart(fig)
+
+    fig2= px.histogram(df_2013,title='Variable CO2 discrète',x='Cat',category_orders=dict(Cat=['A','B','C','D','E','F','G']))
+    st.plotly_chart(fig2)
+    
+ #   from plotly.subplots import make_subplots
+#essais avec subplots
+#    fig3=make_subplots(rows=1,cols=2)
+#    fig3.add_trace(
+#        go.box(df_2013['CO2 (g/km)']),row=1,col=1
+#        )
+#    fig3.add_trace(
+#        go.scatter(x=[20, 30, 40], y=[50, 60, 70]),
+#    row=1, col=2
+#    )
+#    fig.update_layout(height=600, width=800, title_text="Side By Side Subplots")
+#    fig.show()
+#    fig = make_subplots(rows=1, cols=2)
+
+    st.write('### Variables explicatives')
+    st.write('Il y a deux types de variables explicatives : qualitatives et quantitatives')
+
+   
+    var_num_2013 = df_2013.select_dtypes(exclude = 'object') # On récupère les variables numériques
+    var_cat_2013 = df_2013.select_dtypes(include = 'object') # On récupère les variables catégorielles
+
+    tab_num=pd.DataFrame(var_num_2013.columns,columns=['Quantitatives'])
+    tab_cat=pd.DataFrame(var_cat_2013.columns,columns=['Qualitatives'])
+    
+    st.dataframe(pd.concat([tab_num,tab_cat],axis=0))
+
+    st.write('Variables quantitatives')
+
+
+
+    st.write('Variables qualitatives')
+
+    fig_mq= px.histogram(df_2013,title='Répartition par marque',y='Marque').update_yaxes(categoryorder='total ascending')
+    st.plotly_chart(fig_mq)
+    st.write('on observe une sur-représentation de Mercedes-Benz dans le dataframe')
+
+
+
+    st.write('### Etat des lieux des données')
+    st.dataframe(df_2013.info())
+    st.markdown('Beaucoup de valeurs manquantes pour les variables HC (g/km) et HC+NOX (g/km)')
+    
+    if st.checkbox('Afficher les valeurs manquantes'):
+        st.dataframe(df_2013.isna().sum())  
+
+    st.write('#### Description statistique des données')
+    st.dataframe(df_2013.describe())
+    st.markdown('Les ordres de grandeurs sont très larges : il sera nécessaire de procéder à une standardisation des valeurs')
+
+    
+    st.markdown('#### Corrélation des variables selon Pearson')
+    st.dataframe(df_quant.corr())
+    st.markdown("Il apparaît qu'il y a une forte corrélation entre les différents types de consommation et les émissions de CO2")
+    st.markdown("Il y a aussi une corrélation marquée entre les masses des véhicules (min et max) et les émissions de CO2")
+    st.markdown("La variable HC+NOX (g/km) n'a pas été retenue car elle a trop de valeurs manquantes")
+
+
+
+
+
+
 if page == pages[2]:
     st.write('## Analyse des données')
 
-st.markdown("Proposition de plan: Analyse des données \n")
-st.write("   1- Variable à expliquer : CO2, représentation graphique et répartition. traitement continue et discrétisée")
-st.write("   2- Variables explicatives : description")
-st.write("   3- relation entre varaibles explicatives et à expliquer")
-st.write("   4- Choix des variables pour la classification")
-st.write("   5- dédoublonnage final")
 
-st.write("La variable à expliquer : CO2")
-
-
-fig= px.box(df_2013['CO2 (g/km)'],x='CO2 (g/km)',title='Variable CO2 continue',notched=True)
-st.plotly_chart(fig)
-
-st.dataframe(df_2013.cat_poll)
-
-fig2= px.histogram(df_2013,x='Marque')
-st.plotly_chart(fig2)
-
-st.write("Les variables explicatives")
-
-
-#------------------------------------------------------------------------
-#------------------------------------------------------------------------
 if page == pages[3]:
-    st.write('## Visualisation des données')
+    st.write('## Viusalisation des données')
     
     fig = plt.figure(figsize = (10, 5))
-    sns.catplot(data = df_2013, x = 'Carburant', y = 'CO2 (g/km)', kind = 'box', height=5, aspect=2)
-    plt.title("Répartition de la distribution de l émission de CO2 en fonction du type d'énergie");
-    st.pyplot(fig)
-
-#------------------------------------------------------------------------
-#------------------------------------------------------------------------    
+    sns.catplot(x = 'Carburant', y = 'CO2 (g/km)', kind = 'box', height=5, aspect=2, data = df_2013)
+    plt.title("Répartition de la distribution de l émission de CO2 en fonction du type d'énergie")
+    st.pyplot(fig);
+    
 if page == pages[4]:
     
     st.write('## Machine Learning - Classification multiple')
     st.write('### Déterminer la classe du véhicule selon les normes européennes')
+    st.markdown('Les véhicules doivent être classés dans les catégories de A à G (du moins polluant au plus polluant) selon leurs émissions de CO2')
     
     df= pd.read_csv('ML_C02.csv', index_col = 0)
     # Le document ML_02.csv est le DataFrame obtenu après nettoyage et analyse du DataFrame initial df_2013.csv
@@ -191,8 +223,8 @@ if page == pages[4]:
     df = df.rename(variables, axis = 1)
 
     st.markdown('Le DataFrame comporte 5 variables numériques et 10 variables catégorielles')
-     
-
+    st.dataframe(df.info())
+    
     # On sépare les variables numériques et catégorielles
     var_num = df.select_dtypes(exclude = 'object') # On récupère les variables numériques
     var_cat = df.select_dtypes(include = 'object') # On récupère les variables catégorielles
@@ -203,10 +235,13 @@ if page == pages[4]:
 
     # Les variables catégorielles sont transformées en indicatrices
     var_cat_ind = pd.get_dummies(var_cat)
-    var_cat_ind
 
     # On récupère les variables explicatives
     feats = var_num.join(var_cat_ind)
+    
+    st.markdown('Distribution de la variable cible')
+    st.dataframe(target.value_counts(normalize = True))
+    st.markdown('On a une distribution légèrement déséquilibrée')
         
 
     # Commented out IPython magic to ensure Python compatibility.
@@ -239,7 +274,7 @@ if page == pages[4]:
     # %matplotlib inline
     import matplotlib.pyplot as plt
     import seaborn as sns
-
+    
     st.write('### **Cas 1 : Modélisation sans les variables qualitatives**')
 
     # On ne conserve que les variables quantitatives pour effectuer la modélisation
@@ -255,6 +290,8 @@ if page == pages[4]:
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
     
+    st.markdown('Nous procédons à une classification multiple. Nous avons donc choisi les classifieurs adaptés.')
+    st.markdown('Nous en avons sélectionné 3 pour cette étude: SVC, KNN et Random Forest')
     selection_modele = st.selectbox("Choix du modèle", options = ["SVC", "KNN", "Random Forest"])
     
     def train_model(selection_modele):
@@ -271,11 +308,59 @@ if page == pages[4]:
         y_pred = model.predict(X_test)
         score = model.score(X_test, y_test)
         
-        return score
+        st.write('La matrice de confusion obtenue :')
+        matrix = pd.crosstab(y_test, y_pred, rownames = ['Classes réelles'], colnames = ['Classes prédites'])
+        st.dataframe(matrix)
+        
+        if selection_modele == 'Random Forest':
+            feature_scores = pd.Series(model.feature_importances_, index=feats_quant.columns).sort_values(ascending=False)
+            fig = plt.figure(figsize = (10, 5))
+            sns.barplot(x=feature_scores, y=feature_scores.index)
+            plt.xlabel('Feature Importance Score')
+            plt.ylabel('Features')
+            plt.title("Visualisation des features les plus importants")
+            st.pyplot(fig);
+            
+        return score.round(2)
     
     st.write("L'accuracy du classifieur choisi est de", train_model(selection_modele))
+    
+    st.write("###### Voting Classifier")
+    
+    # Classifieur SVC
+    clf_svc2 = SVC(gamma = 'scale')        # Instanciation du classifieur
+    clf_svc2.fit(X_train, y_train)         # Entraînement du classifieur
+    y_pred_svc2 = clf_svc2.predict(X_test) # Prédictions du classifieur
+
+
+    # Classifieur KNN
+    clf_knn2 = KNeighborsClassifier()       # Instanciation du classifieur
+    clf_knn2.fit(X_train, y_train)          # Entraînement du classifieur
+    y_pred_knn2 = clf_knn2.predict(X_test)  # Prédictions du classifieur
+    
+    # Classifieur Random Forest
+    clf_rf2 = RandomForestClassifier()     # Instanciation du classifieur
+    clf_rf2.fit(X_train, y_train)          # Entraînement du classifieur
+    y_pred_rf2 = clf_rf2.predict(X_test)   # Prédictions du classifieur
+
+    # Voting Classifier
+    clf_vc2 = VotingClassifier([('rf', clf_rf2), ('svc', clf_svc2), ('knn', clf_knn2)], voting = 'hard')
+
+    # Création du cross-validator
+    cv3 = KFold(n_splits = 3) # Question : comment choisir les autres paramètres du CV? Comment définir le nombre optimal de splits?
+
+    st.markdown('Validation croisée et évaluation des classifieurs')
+    for clf, label in zip([clf_rf2, clf_svc2, clf_knn2, clf_vc2], ['Random Forest', 'SVC', 'KNN', 'Voting Classifier']):
+        scores = cross_validate(clf, feats_quant, target, cv=cv3, scoring=['accuracy','f1_weighted'])
+        st.write("[%s]: \n Accuracy: %0.2f (+/- %0.2f)" % (label, scores['test_accuracy'].mean(), scores['test_accuracy'].std()),
+              "F1 score: %0.2f (+/- %0.2f)" % (scores['test_f1_weighted'].mean(), scores['test_f1_weighted'].std()))
+
+    st.write('Suite au Voting Classifier, on constate que le modèle Random Forest est celui qui donne de bien meilleurs résultats')
+
+    
     '''
     st.write("##### Optimisation des hyperparamètres du classifieur choisi")
+    
     def optim_hyper(selection_modele):
         if selection_modele == "SVC":
             model = SVC(gamma = 'scale')
